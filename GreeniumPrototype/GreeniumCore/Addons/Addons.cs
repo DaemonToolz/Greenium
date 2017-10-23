3355999ce5df65c21da4acca7f7f5f76779dee5f
DaemonToolz <axel.maciejewski@viacesi.fr>
2017-10-23 16:24:58 +0200

    Client Improvement • Plugin Reader Alpha

---
+++ GreeniumPrototype/GreeniumCore/Addons/Addons.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreeniumCore.Addons {
+    public class Addons {
+        public List<DllReader> MyAddons { get; set; }
+    }
+}
---
+++ GreeniumPrototype/GreeniumCore/Addons/DllReader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using GreeniumCommon;
+
+namespace GreeniumCore.Addons {
+    public class DllReader {
+        private String Path { get; set; }
+
+        public AssemblyName Name { get; private set; }
+        public Assembly LoadedAssembly { get; private set; }
+
+        public Type PType { get; private set; }
+        private Boolean IsLoaded { get; set; } = false;
+        private Type[] PluginTypes { get; set; }
+
+        public DllReader(String path, bool load = false){
+            Path = path;
+            if (load)
+                Load();
+            
+        }
+
+        public bool Load(){
+            if (IsLoaded) return IsLoaded;
+            try {
+                Name = AssemblyName.GetAssemblyName(Path);
+                LoadedAssembly = Assembly.Load(Name);
+                PType = typeof(IPlugin);
+                IsLoaded = (LoadedAssembly != null);
+            } catch {}
+
+            return IsLoaded;
+        }
+
+        private Type[] GetTypes(){
+            if (IsLoaded)
+                return PluginTypes = LoadedAssembly.GetTypes();
+            return null;
+        }
+
+        private IPlugin Generate(Type type){
+            if (IsLoaded && type.GetInterface(PType.FullName) != null)
+                return (IPlugin) Activator.CreateInstance(type);
+            return null;
+        }
+    }
+}
