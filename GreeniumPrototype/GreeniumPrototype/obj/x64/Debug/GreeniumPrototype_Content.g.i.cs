59d0e9bf5ca20e820f5cfc4a9047f3fc213c418f
DaemonToolz <axel.maciejewski@viacesi.fr>
2017-12-08 11:35:06 +0100

    Possibilité d'évolution - Exemple Greenium Gaming

--- GreeniumPrototype/GreeniumPrototype/obj/x64/Debug/GreeniumPrototype_Content.g.i.cs
+++ GreeniumPrototype/GreeniumPrototype/obj/x64/Debug/GreeniumPrototype_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/plussign.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/notreselection.png")]
 
 
